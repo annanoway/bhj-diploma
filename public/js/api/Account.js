6c069b03e58ac3841e2eb49cc1d80f0a2b708110
Анна Шестакова <annanoway7@gmail.com>
2022-09-10 12:27:31 +0300

    account

--- public/js/api/Account.js
+++ public/js/api/Account.js
@@ -7,7 +7,13 @@ class Account extends Entity {
   /**
    * Получает информацию о счёте
    * */
-  static get(id = '', callback){
-
-  }
+   static get(id = '', callback) {
+    createRequest({
+      url: this.URL + '/' + id,
+      method: 'GET',
+      callback,
+      });
+    }
 }
+// http://localhost:8000/account
+Account.URL = '/account';
